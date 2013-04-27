35dda671779a6fc13bb6511cf4c7624c6182cebf
Gyula Krajczar <gyula.krajczar@gmail.com>
2013-04-27 15:12:05 +0200

    Analysis step module refactor

--- WebContent/resources/script/visan/modules/analysisstepmodule.js
+++ WebContent/resources/script/visan/modules/analysisstepmodule.js
@@ -39,6 +39,70 @@ function analysisStepModule(workingArea, visan, options) {
 		console.log(plotDialog.dialog("option", "position"));
 	}
 	
+	function newPlotEventHandler() {
+		
+		var dialog = workingArea.find("div#analysis-step-new-plot-dialog").clone();
+		var plotTypeSelect = dialog.find("select#new-plot-type");
+		for(var type in plottypes) {
+			plotTypeSelect.append($("<option />").attr("value", type).text(plottypes[type].displayName));
+		}
+		
+		console.log("initialized plot type select");
+		
+		var optionsContainer = dialog.find("div#new-plot-options");
+		var plotCallback = undefined;
+		
+		plotTypeSelect.change(function() {
+			// find current selection and render options
+			var plotType = $(this).find("option:selected").val();
+			var plotClassName = (plotType != "empty" ? plottypes[plotType].classname : "empty");
+			
+			// do nothing if "empty" is selected
+			if(plotClassName == "empty") return;
+			
+			plotCallback = window[plotClassName].renderPlotCreateOptions(optionsContainer);
+			
+			optionsContainer.find("select.axis").each(function() {
+				var axesSelect = $(this);
+				headers.forEach(function(item, index) {
+					$("<option />").val(item).text(item).appendTo(axesSelect);
+				});
+			});
+		});
+		
+		dialog.dialog({
+			modal: true,
+			buttons: {
+				"Create plot": function() {
+					var plotTitle = dialog.find("input#new-plot-title").val();
+					var plotType = plotTypeSelect.find("option:selected").val();
+					
+					if(plotType == "empty") return;
+					
+					var plotOptions = {
+						title: plotTitle,
+						type: plotType,
+						width: 500,
+						height: 500,
+						top: 10,
+						left: 100
+					};
+					
+					plotCallback(plotOptions, optionsContainer);
+					
+					analysis.plots.push(plotOptions);
+					
+					$(this).dialog("close");
+					
+					createPlot(plotOptions);
+				},
+				"Cancel": function() {
+					$(this).dialog("close");
+				}
+			}
+		});
+	};
+	
 	visan.title((typeof analysis.title == "undefined" ? "Untitled analysis" : analysis.title));
 	
 	visan.loadTemplate("analysis-step", function(template) {
@@ -50,68 +114,6 @@ function analysisStepModule(workingArea, visan, options) {
 			createPlot(plotOptions);
 		});
 		
-		workingArea.find("button#analysis-step-new-plot").button().click(function() {
-			
-			var dialog = workingArea.find("div#analysis-step-new-plot-dialog").clone();
-			var plotTypeSelect = dialog.find("select#new-plot-type");
-			for(var type in plottypes) {
-				plotTypeSelect.append($("<option />").attr("value", type).text(plottypes[type].displayName));
-			}
-			
-			console.log("initialized plot type select");
-			
-			var optionsContainer = dialog.find("div#new-plot-options");
-			var plotCallback = undefined;
-			
-			plotTypeSelect.change(function() {
-				// find current selection and render options
-				var plotType = $(this).val();
-				var plotClassName = (plotType != "empty" ? plottypes[type].classname : "empty");
-				
-				// do nothing if "empty" is selected
-				if(plotClassName == "empty") return;
-				
-				plotCallback = window[plotClassName].renderPlotCreateOptions(optionsContainer);
-				
-				optionsContainer.find("select.axis").each(function() {
-					var axesSelect = $(this);
-					headers.forEach(function(item, index) {
-						$("<option />").val(item).text(item).appendTo(axesSelect);
-					});
-				});
-			});
-			
-			dialog.dialog({
-				modal: true,
-				buttons: {
-					"Create plot": function() {
-						var plotTitle = dialog.find("input#new-plot-title").val();
-						var plotClassName = plotTypeSelect.val();
-						
-						if(plotClassName == "empty") return;
-						
-						var plotOptions = {
-							title: plotTitle,
-							type: plotType,
-							width: 500,
-							height: 500,
-							top: 10,
-							left: 100
-						};
-						
-						plotCallback(plotOptions, optionsContainer);
-						
-						analysis.plots.push(plotOptions);
-						
-						$(this).dialog("close");
-						
-						createPlot(plotOptions);
-					},
-					"Cancel": function() {
-						$(this).dialog("close");
-					}
-				}
-			});
-		});
+		workingArea.find("button#analysis-step-new-plot").button().click(newPlotEventHandler);
 	});
 };
